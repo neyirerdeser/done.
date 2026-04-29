61abd6ab6e3eaeceedb0353464ac092fd1bc0cf5
neyirerdeser <neyirerdeser@gmail.com>
2026-04-29 16:33:57 -0400

    made the app production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 
 import { connectDB } from "./config/db.js"
 import usersRoutes from "./routes/usersRoutes.js"
@@ -11,11 +12,14 @@ import itemsRoutes from "./routes/itemsRoutes.js"
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 // MIDDLEWARE
-app.use(cors({
-    origin: process.env.FRONTEND_URL
-}))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: process.env.FRONTEND_URL
+    }))
+}
 app.use(express.json()) // ie bodyParser
 
 // ROUTES
@@ -29,6 +33,14 @@ app.use((error, _, res, next) => {
     res.status(error.code || 500);
     res.json({ message: error.message || "unknown error. sorry :(" });
 });
+
+// SERVE THE FRONTEND
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 // LISTEN
 connectDB(process.env.MONGO_URI).then(() => {
